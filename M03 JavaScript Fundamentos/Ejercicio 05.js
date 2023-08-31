e1dabddb88a7e2fa52b381874a223b887650c394
leoparra19 <leandroparravichini@gmail.com>
2023-08-30 23:06:17 -0300

    commit 3444

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -21,6 +21,7 @@ function agregarSimboloExclamacion(str) {
    // Agrega un símbolo de exclamación al final del string "str" y retórnalo
    // Ejemplo: "hello world" ---> "hello world!"
    // Tu código:
+   return (str + "!");
 }
 
 function combinarNombres(nombre, apellido) {
