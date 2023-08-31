7833be026dd6d173acc21283dec127bf38a41075
leoparra19 <leandroparravichini@gmail.com>
2023-08-30 23:34:58 -0300

    commit aas

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -35,7 +35,7 @@ function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
-   return ('Hola ' + nombre);
+   return 'Hola ' + nombre; 
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
