f8f8000412ad638a23ccb47080d583b7e8cd31bd
leoparra19 <leandroparravichini@gmail.com>
2023-08-31 20:26:03 -0300

    commit 021

--- demo.js
+++ demo.js
@@ -1,7 +1,11 @@
-function cuentaRegresiva(num){     // num = 5
-for(var i = num; i >= 0; i--){
+//        BUCLE FOR         //
+
+function cuentaRegresiva(num){     // Por ejemplo el 9: entra en el for y se reemplaza i=9; 9 es >= 0? si, entonces immprime el 9 y luego ; i-- decrementa un punto y reinicia el blucle 
+for(var i = num  ; i >= 0 ; i--){     // 
     console.log(i);
 }
 }
 
-cuentaRegresiva(5)
+cuentaRegresiva                    (9)
+//invocacion de la funcion          //argumento(x)
+
