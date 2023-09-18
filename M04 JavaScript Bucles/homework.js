40cdb4f256c3e38733b0462028a41637273dca3f
leoparra19 <leandroparravichini@gmail.com>
2023-09-18 15:59:18 -0300

    commit asdasd

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,13 +5,22 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
-}
+   if(x>y){
+      return x;
+   }else if(x<y){
+      return y;
+   }else (x===y); return x;
+   }
+
 
 function mayoriaDeEdad(edad) {
    // Determinar si la persona puede ingresar al evento según su edad.
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
+   if(edad>18){
+      return "Allowed";
+   }else return "Not allowed";
 }
 
 function conection(status) {
@@ -21,6 +30,7 @@ function conection(status) {
    // De lo contrario, presumimos que el usuario está "Offline".
    // Retornar el estado de conexión del usuario.
    // Tu código:
+
 }
 
 function saludo(idioma) {
